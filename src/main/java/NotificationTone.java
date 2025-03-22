3b25d91dc9f8794eedac5fafd2735a23610ea744
marticas-ful <141726695+marticas-ful@users.noreply.github.com>
2025-03-22 22:37:38 +0100

    Added 'NotificationTone' class
    Class that plays notification tone whenever a message is sent and received. Very standard/short class

---
+++ src/main/java/NotificationTone.java
@@ -0,0 +1,21 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class NotificationTone {
+    public static void playNotificationTone(String soundFile) {
+        try {
+            File file = new File(soundFile);
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
